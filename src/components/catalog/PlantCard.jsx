16975c3082e6cfd26902df7173a17f1935ec68ca
KingSeraph07 <runsewemichael93@gmail.com>
2025-08-09 01:53:34 +0100

    about

--- src/components/catalog/PlantCard.jsx
+++ src/components/catalog/PlantCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const PlantCard = ({ plant }) => {
   return (
-    <div className="card h-100 bg-dark border border-success shadow-sm" style={{borderRadius: "10px"}}>
+    <div className="card h-100 bg-dark border border-success shadow-sm" style={{borderRadius: "10px", border:'2px solid var(--accent-color)'}}>
       <img src={plant.image} className="card-img-top" alt={plant.name} />
       <div className="card-body">
         <h5 className="card-title text-success fw-bold">{plant.name}</h5>
